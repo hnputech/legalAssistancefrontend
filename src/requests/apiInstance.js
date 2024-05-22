8c23cadb26530f5203130c119580cbe105289618
huzafa-arshad-joulea <huzafa.arshad@joule.com>
2024-05-22 16:00:34 +0500

    change backedn path

--- src/requests/apiInstance.js
+++ src/requests/apiInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const axioxInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  // baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://legalai-aondtyyl6a-uc.a.run.app",
   headers: {
     "Content-Type": "application/json",
   },
--- src/requests/chats.js
+++ src/requests/chats.js
@@ -6,7 +6,7 @@ export const chats = async (chat) => {
   const jsonchats = JSON.stringify(chat);
   console.log("====jsonchats", jsonchats);
   try {
-    const response = await axioxInstance.post("/chat", jsonchats);
+    const response = await axioxInstance.post("/chat/", jsonchats);
     return response.data;
   } catch (err) {
     console.log("erro");
